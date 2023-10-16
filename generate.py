2fabbde7d377a7776b23db9a148a670a408273e7
Mihnea Mihai <mihnea.mihai@gmx.com>
2023-10-16 22:59:07 +0300

    revamp

--- generate.py
+++ generate.py
@@ -1,19 +1,21 @@
-from time import sleep
-from jinja2 import Environment, FileSystemLoader
+"""This generates the static HTML file from the JSON resume file."""
 import json
 
+from time import sleep
+from jinja2 import Environment, FileSystemLoader
+
 env = Environment(
-    loader=FileSystemLoader('templates'),
-    lstrip_blocks=True,
-    trim_blocks=True
-    )
+    loader=FileSystemLoader("templates"), lstrip_blocks=True, trim_blocks=True
+)
 while True:
-    with open('resume.json') as file:
+    with open("resume.json", encoding="utf-8") as file:
         resume = json.load(file)
 
-    with open('resume.html', 'w') as file:
-        file.write(env.get_template('resume.html').render(
-            resume, trim_blocks = True, lstrip_blocks=True
-            ))
+    with open("resume.html", "w", encoding="utf-8") as file:
+        file.write(
+            env.get_template("resume.html").render(
+                resume, trim_blocks=True, lstrip_blocks=True
+            )
+        )
 
     sleep(1)
